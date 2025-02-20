7720f8b0462ffa5d9e947a75289d3b7d82de7c26
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2025-02-20 17:28:09 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+int port = 8080;
+
+var listener = new TcpListener(IPAddress.Any, port);
+listener.Start();
+
+Console.WriteLine($"Balancer started on port {port}");
+
